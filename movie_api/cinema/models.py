bb4ac4f053d203f021b8bb2d61039b2966f1b7cb
Alexey <alopar632@gmail.com>
2025-07-09 17:30:01 +0300

    Add Movie model, views, and .gitignore

---
+++ movie_api/cinema/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+
+    def __str__(self):
+        return self.title
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+
+    def __str__(self):
+        return self.title
---
+++ movie_api/cinema/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, viewsets
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+class MovieListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
