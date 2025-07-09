3d5e4abd81048d076b407b9c194dfcf0b166709e
Alexey <alopar632@gmail.com>
2025-07-09 17:50:41 +0300

    Fix: add release_date to MovieSerializer to match model

--- movie_api/cinema/serializers.py
+++ movie_api/cinema/serializers.py
@@ -4,4 +4,4 @@ from .models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description', 'duration']
+        fields = ['id', 'title', 'description', 'release_date', 'duration']
